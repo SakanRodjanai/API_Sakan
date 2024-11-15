7ec25f0374952a151ad34297a72d0817e62f7ab9
Sakan <sakan.rodjanai@gmail.com>
2024-11-15 19:39:11 +0700

    Complete

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get("https://api.adviceslip.com/advice")
+        res.render("index.ejs", { advi: result.data.slip.advice, });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }   
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on port ${port}.`)
+})
